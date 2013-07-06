edf585409567ca89f22a0e0cb7c2377f1a7c9619
Edson Lima <edsonlima@edson-notebook.(none)>
2013-07-06 10:57:01 -0300

    primeiro commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Movimentobancario::Application.config.secret_token = '1f94c86b0a6976654dd7fd3e09261e7298ea5a5baf340aa7b2ede33dad1e80452a3e47fd435f66399c33707ad501621935b44424b185d014bd976785c09896fd'
---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Movimentobancario::Application.routes.draw do
+  root :to => 'pages#index'
+
+  get "pages/index"
+
+  devise_for :users
+  
+end
