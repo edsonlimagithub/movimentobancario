36b6d0ea7c5cb1b27ca4983e005bcc8f383a4ca6
Edson Lima <edsonlima@edson-notebook.(none)>
2013-12-21 10:21:39 -0300

    exibindo todos os erros na produção

--- app/models/sitef_file.rb
+++ app/models/sitef_file.rb
@@ -2,7 +2,8 @@ class SitefFile < ActiveRecord::Base
   attr_accessible :arquivo, :dia_correspondente
   has_attached_file :arquivo,
                   :url  => "/public/arquivos_sitef/arquivo_sitef.prn",
-                  :path => ":rails_root/public/arquivos_sitef/arquivo_sitef.prn"
+                  :path => ":rails_root/public/arquivos_sitef/arquivo_sitef.prn",
+                  :s3_permissions => "public-read"
 
 validates_attachment_presence :arquivo
 validates_attachment_size :arquivo, :less_than => 5.megabytes
