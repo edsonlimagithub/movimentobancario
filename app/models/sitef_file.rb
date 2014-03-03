617a8c6d29bfef6409b4833bd10aa18b749304ce
Edson Lima <edsonlima@edson-notebook.(none)>
2014-03-03 10:31:15 -0300

    erro na importação do arquivo

--- app/models/sitef_file.rb
+++ app/models/sitef_file.rb
@@ -2,7 +2,7 @@ class SitefFile < ActiveRecord::Base
   attr_accessible :arquivo, :dia_correspondente
   has_attached_file :arquivo,
                   :url  => "/public/arquivos_sitef/arquivo_sitef.prn",
-                  :path => ":rails_root/public/arquivos_sitef/arquivo_sitef.prn",
+                  :path => ":rails_root/public/arquivos_sitef/arquivo_sitef.prn"
 
 validates_attachment_presence :arquivo
 validates_attachment_size :arquivo, :less_than => 5.megabytes
